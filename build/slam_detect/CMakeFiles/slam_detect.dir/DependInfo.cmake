2ddf35353188b1f3282d16b9a20a3fa875c1fb81
nemo <log.club.sonia@etsmtl.net>
2025-11-27 12:54:40 -0500

    addition of 3D sort

---
+++ build/slam_detect/CMakeFiles/slam_detect.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sonia/ros2_sonia_ws/src/slam_detect/src/SlamDetect.cpp" "CMakeFiles/slam_detect.dir/src/SlamDetect.cpp.o" "gcc" "CMakeFiles/slam_detect.dir/src/SlamDetect.cpp.o.d"
+  "/home/sonia/ros2_sonia_ws/src/slam_detect/src/main.cpp" "CMakeFiles/slam_detect.dir/src/main.cpp.o" "gcc" "CMakeFiles/slam_detect.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
